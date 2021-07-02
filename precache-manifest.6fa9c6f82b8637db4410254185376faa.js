a6af082c69a689db51f43e60c072ae581216bb45
Mikegor01 <72784407+Mikegor01@users.noreply.github.com>
2021-07-02 17:48:43 +0900

    Updates

---
+++ precache-manifest.6fa9c6f82b8637db4410254185376faa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1803586f958736347cfa06238ac7faf3",
+    "url": "/Capputoken/index.html"
+  },
+  {
+    "revision": "118847ea6e2834e8f75c",
+    "url": "/Capputoken/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "69ec8900c54ee1830fa5",
+    "url": "/Capputoken/static/js/2.6603f368.chunk.js"
+  },
+  {
+    "revision": "118847ea6e2834e8f75c",
+    "url": "/Capputoken/static/js/main.59245ecf.chunk.js"
+  },
+  {
+    "revision": "568feaee3bea73fdfae9",
+    "url": "/Capputoken/static/js/runtime-main.b293f015.js"
+  }
+]);
